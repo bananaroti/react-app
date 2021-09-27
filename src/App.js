7dbdbe665269e04ce092d7e1cc8a38fb69c43544
bananaroti <gh3298@gmail.com>
2021-09-27 14:05:51 +0900

    src - components
    app.js에있는 컴포넌트들을 폴더에 분리해주고 임포트 해줌

--- src/App.js
+++ src/App.js
@@ -1,47 +1,8 @@
 import "./App.css";
 import { Component } from "react";
-
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        <p>{this.props.sub}</p>
-      </header>
-    );
-  }
-}
-
-class Toc extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <a href="1.html">HTML</a>
-          </li>
-          <li>
-            <a href="2.html">CSS</a>
-          </li>
-          <li>
-            <a href="3.html">JavaScript</a>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component {
-  render() {
-    return (
-      <article>
-        <h2>{this.props.title}</h2>
-        <p>{this.props.desc}</p>
-      </article>
-    );
-  }
-}
+import Subject from "./components/Subject";
+import Toc from "./components/TOC";
+import Content from "./components/Content";
 
 class App extends Component {
   render() {
