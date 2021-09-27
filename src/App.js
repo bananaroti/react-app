4a0ed7423b36a3fc8d099b22093604864c263832
bananaroti <gh3298@gmail.com>
2021-09-27 16:22:24 +0900

    Constructor, state

--- src/App.js
+++ src/App.js
@@ -5,14 +5,37 @@ import Toc from "./components/TOC";
 import Content from "./components/Content";
 
 class App extends Component {
+  // 컴포넌트가 실행돨 때 컨스트럭터가 실행되서 초기화를 담당함
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      Subject: {
+        title: "WEB",
+        sub: "World wide Web!",
+      },
+      Toc: [
+        { id: 1, title: "HTML", desc: "HTML is for information" },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "JavaScript", desc: "JavaScript is for interactive" },
+      ],
+      Content: {
+        title: "HTML",
+        desc: "HTML is HyperText Markup Language.",
+      },
+    };
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="World wide web!"></Subject>
-        <Toc></Toc>
+        <Subject
+          title={this.state.Subject.title}
+          sub={this.state.Subject.sub}
+        ></Subject>
+        <Toc data={this.state.Toc}> </Toc>
         <Content
-          title="HTML"
-          desc="HTML is HyperText Markup Language."
+          title={this.state.Content.title}
+          desc={this.state.Content.desc}
         ></Content>
       </div>
     );
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,6 +2,17 @@ import { Component } from "react";
 
 class Toc extends Component {
   render() {
+    var lists = [];
+    var data = this.props.data;
+    var i = 0;
+    while (i < data.length) {
+      lists.push(
+        <li key={data[i].id}>
+          <a href={"/content/" + data[i].id}>{data[i].title}</a>
+        </li>
+      );
+      i += 1;
+    }
     return (
       <nav>
         <ul>
